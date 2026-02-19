6d05fbb6fc0cacfcd835583827a2276e172d9484
DanD <danielduarte8925@gmail.com>
2026-02-19 12:34:55 -0500

    Datos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Daniel Andres Durate Aguirre");
+        System.out.println("Cota Cundinamarca");
+        System.out.println("20");
+        System.out.println("2 Perros Mandarina y Zafir");
 
         }
     }
